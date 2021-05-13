87d12378afa0e8585b20f1aea67820d253ce6950
John Fulton <john@techelevator.com>
2021-05-13 13:36:16 -0400

    module-1/04_Loops_Arrays/lecture-final/

---
+++ module-1/04_Loops_Arrays/lecture-final/dotnet/Lecture/03_ReturnLastElement.cs
@@ -0,0 +1,22 @@
+﻿namespace Lecture
+{
+    public partial class LectureProblem
+    {
+        /*
+        3. Return the last element of the array
+            TOPIC: Accessing Array Elements
+        */
+        public int ReturnLastElement()
+        {
+            int[] portNumbers = { 80, 8080, 443 };
+
+            // VARIABLE that references the last Index
+            int lastIndex = portNumbers.Length - 1;
+
+            // VARIABLE that references the last element
+            int lastElement = portNumbers[portNumbers.Length - 1];
+
+            return lastElement;
+        }
+    }
+}
