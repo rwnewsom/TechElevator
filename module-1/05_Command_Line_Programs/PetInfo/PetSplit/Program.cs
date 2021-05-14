7b8e2285d844422ee093c40f346012cb1f0076e2
John Fulton <john@techelevator.com>
2021-05-14 14:42:00 -0400

    module-1/05_Command_Line_Programs/ additional

---
+++ module-1/05_Command_Line_Programs/PetInfo/PetSplit/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PetSplit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter several values, seperated by a space.");
+            string userInput = Console.ReadLine();
+
+            string[] words = userInput.Split(' ');
+
+            for(int i = 0; i < words.Length; i++)
+            {
+                Console.WriteLine((i + 1) + " - " + words[i]);
+            }
+
+            return;
+        }
+    }
+}
