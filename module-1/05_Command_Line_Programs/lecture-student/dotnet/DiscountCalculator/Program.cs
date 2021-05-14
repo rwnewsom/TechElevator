b267202b9ca2d209bbbbd80c16b62ffc11ea9f3a
John Fulton <john@techelevator.com>
2021-05-13 20:39:11 -0400

    module-1/05_Command_Line_Programs/

---
+++ module-1/05_Command_Line_Programs/lecture-student/dotnet/DiscountCalculator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DiscountCalculator
+{
+    class Program
+    {
+        /// <summary>
+        /// The main method is the start and end of our program.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Discount Calculator");
+
+            // Prompt the user for a discount amount
+            // The answer needs to be saved as a double
+            Console.Write("Enter the discount amount (w/out percentage): ");
+
+
+
+            // Prompt the user for a series of prices
+            Console.Write("Please provide a series of prices (space separated): ");
+
+
+
+
+
+
+        }
+    }
+}
