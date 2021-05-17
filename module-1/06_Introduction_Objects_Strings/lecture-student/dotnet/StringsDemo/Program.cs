e6f996fb52d23022b9d0ca8634cf10b274076c24
Matt Eland <matt.eland@gmail.com>
2021-05-16 21:40:43 -0400

    Clarified some steps on the lecture code

--- module-1/06_Introduction_Objects_Strings/lecture-student/dotnet/StringsDemo/Program.cs
+++ module-1/06_Introduction_Objects_Strings/lecture-student/dotnet/StringsDemo/Program.cs
@@ -15,42 +15,40 @@ namespace StringsDemo
             //      of name.
             // Output: A
             // Output: e
-
-            // Console.WriteLine("First and Last Character. ");
+            //Console.WriteLine("First and Last Character: ");
 
             // 2. How do we write code that prints out the first three characters
             // Output: Ada
 
-            // Console.WriteLine("First 3 characters: ");
+            //Console.WriteLine("First 3 characters: ");
 
-            // 3. Now print out the first three and the last three characters
-            // Output: Adaace
+            // 3. Now print out the last three characters
+            // Output: ace
 
-            // Console.WriteLine("Last 3 characters: ");
+            //Console.WriteLine("Last 3 characters: ");
 
             // 4. What about the last word?
             // Output: Lovelace
 
-            // Console.WriteLine("Last Word: ");
+            //Console.WriteLine("Last Word: ");
 
             // 5. Does the string contain inside of it "Love"?
             // Output: true
 
-            // Console.WriteLine("Contains \"Love\"");
+            //Console.WriteLine("Contains \"Love\"");
 
             // 6. Where does the string "lace" show up in name?
             // Output: 8
 
-            // Console.WriteLine("Index of \"lace\": ");
+            //Console.WriteLine("Index of \"lace\": ");
 
             // 7. How many 'a's OR 'A's are in name?
             // Output: 3
 
-            // Console.WriteLine("Number of \"a's\": ");
+            //Console.WriteLine("Number of \"a's\": ");
 
             // 8. Replace "Ada" with "Ada, Countess of Lovelace"
-
-            // Console.WriteLine(name);
+            //Console.WriteLine(name);
 
             // 9. Set name equal to null.
 
