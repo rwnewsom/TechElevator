76a9854b23881c99cd72d460171adcdba1147460
Matt Eland <matt.eland@gmail.com>
2021-05-18 17:05:12 -0400

    module-1/08_Collections_Part_2 Exercise and Lecture Code

---
+++ module-1/08_Collections_Part_2/lecture-student/dotnet/CollectionsPart2Lecture/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CollectionsPart2Lecture
+{
+    public class CollectionsPart2Lecture
+	{
+        static void Main(string[] args)
+        {
+			// ----------------------------------------
+			// Dictionaries
+			// ----------------------------------------
+
+			// Declaring and initializing a Dictionary
+
+			// Adding an item to a Dictionary
+
+			// Retrieving an item from a Dictionary by its key
+
+			// Updating an item in a Dictionary
+
+			// Check to see if a key already exists
+
+			// Remove an element from the Dictionary
+
+			// ----------------------------------------
+			// Less Common Cases with Dictionaries
+			// ----------------------------------------
+
+			// Declaring and initializing a Dictionary (one-line version)
+
+			// Iterate through the key-value pairs in the Dictionary (You will RARELY do this)
+
+			// Retrieving just the keys from a Dictionary (You will RARELY do this)
+
+			// Null Reference Exceptions
+
+			// Case Sensitivity in Keys
+
+			// ----------------------------------------
+			// HashSets
+			// ----------------------------------------
+		}
+	}
+}
