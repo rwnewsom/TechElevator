584ba76af6f1b21f63d2bc768382a53ed40f7c34
Matt Eland <matt.eland@gmail.com>
2021-05-20 13:58:14 -0400

    module-1/09_Classes_Encapsulation Lecture Final

---
+++ module-1/09_Classes_Encapsulation/lecture-final/dotnet/EncapsulationLecture/GameExamples/Player.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EncapsulationLecture.GameExamples
+{
+    public class Player
+    {
+        // Properties
+        public string Name { get; } // Can ONLY be set in the constructor
+        public int Health { get; private set; }
+        public int ExperiencePoints { get; private set; }
+
+        // Derived Property
+        public bool IsDead
+        {
+            get
+            {
+                if (this.Health <= 0)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+        }
+
+        // Constructor
+        public Player(string name)
+        {
+            Console.WriteLine("Player Constructor Called");
+
+            this.Health = 100;
+            this.Name = name; // Make sure the casing is correct here
+        }
+
+        // Methods
+        public int TakeDamage(int damageAmount)
+        {
+            /*
+            this.Health -= damageAmount;
+            Console.WriteLine("Health is now " + this.Health);
+
+            return this.Health;
+            */
+            return ModifyHealth(-damageAmount);
+        }
+
+        public int Heal(int amountToHeal)
+        {
+            /*
+            this.Health += amountToHeal;
+            Console.WriteLine("Health is now " + this.Health);
+
+            return this.Health;
+            */
+            return ModifyHealth(amountToHeal);
+        }
+
+        private int ModifyHealth(int amountToChange)
+        {
+            this.Health += amountToChange;
+            Console.WriteLine("Health is now " + this.Health);
+
+            return this.Health;
+        }
+    }
+}
---
+++ module-1/09_Classes_Encapsulation/lecture-final/dotnet/EncapsulationLecture/Program.cs
@@ -0,0 +1,67 @@
+﻿//using EncapsulationLecture.CarExamples;
+using EncapsulationLecture.CarExamples;
+using EncapsulationLecture.GameExamples;
+using System;
+
+namespace EncapsulationLecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Car -------------------------
+
+            Car myCar = new Car();
+            myCar.Model = "Civic";
+
+            myCar.Honk();
+
+            double miles = myCar.Drive(22);
+            miles = myCar.Drive(80);
+            Console.WriteLine("The car has driven " + myCar.Odometer + " miles");
+
+            double amount = myCar.Crash(17000);
+            Console.WriteLine("The insurance will give you " + amount);
+
+            Car farrisCar = new Car();
+            farrisCar.Model = "Mustang";
+            farrisCar.Drive(100);
+            farrisCar.Drive(350);
+            farrisCar.Drive(500);
+            // farrisCar.Odometer = 0; Won't work because Odometer has a private set
+            Console.WriteLine("Farris Beuler's Odometer is " + farrisCar.Odometer);
+
+            // Classes
+            // Methods - Drive, FillGas
+            // Properties - IsEngineOn, MilesDriven
+            // Public / Private
+            // Properties with Private Sets
+
+            // Player in a Game -------------
+            Player myPlayer = new Player("Handsome Jack");
+
+            myPlayer.TakeDamage(10);
+
+            Console.WriteLine("Is the player dead? " + myPlayer.IsDead);
+            myPlayer.TakeDamage(25);
+            myPlayer.Heal(50);
+
+            Console.WriteLine("Is the player dead? " + myPlayer.IsDead);
+
+            myPlayer.TakeDamage(200);
+
+            Console.WriteLine("Is the player dead? " + myPlayer.IsDead);
+
+            // myPlayer.Name = "Adventure Girl";
+
+            // Class Documentation
+            // Methods / Properties - Health, Damage, Heal
+            // Constructors
+            // This
+            // Get-Only Properties
+            // Void Methods
+        }
+    }
+}
