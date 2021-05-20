8677a64eaa491a46949c2025741f2064f356c72e
Matt Eland <matt.eland@gmail.com>
2021-05-19 22:29:55 -0400

    module-1/09_Classes_Encapsulation Lecture TODOs

--- module-1/09_Classes_Encapsulation/lecture-student/dotnet/EncapsulationLecture/Program.cs
+++ module-1/09_Classes_Encapsulation/lecture-student/dotnet/EncapsulationLecture/Program.cs
@@ -7,6 +7,21 @@ namespace EncapsulationLecture
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            // Car -------------------------
+            // Classes
+            // Methods - Drive, FillGas
+            // Properties - IsEngineOn, MilesDriven
+            // Public / Private
+            // Properties with Private Sets
+
+            // Player in a Game -------------
+            // Class Documentation
+            // Methods / Properties - Health, Damage, Heal
+            // Constructors
+            // This
+            // Get-Only Properties
+            // Void Methods
         }
     }
 }
