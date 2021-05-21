f327522abbfe22f9cdb385e737802f3d3fe73156
Matt Eland <matt.eland@gmail.com>
2021-05-20 22:27:49 -0400

    module-1/10_Review_Day Lecture Code

---
+++ module-1/10_Week_2_Review/student-lecture/Week2Review/Week2Review/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Week2Review
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            // 1. Add a VotingMachine class, but with documentation this time!
+            // 2. Allow the user to vote for someone (class variables)
+            // 3. Allow multiple people to vote
+            // 4. Calculate a winner
+            // 5. Safeguard Democracy by checking to see if voting is still open (default values)
+            // 6. Incentivize voting by allowing some votes to be worth more than others at random (overloading)
+            // 7. What if we wanted candidates to start on the ballot (in the dictionary)?
+            // 8. Add a "Voting provided by X" static method, go over System.Math
+
+            Console.WriteLine("Thank you for voting! I love democracy.");
+        }
+    }
+}
