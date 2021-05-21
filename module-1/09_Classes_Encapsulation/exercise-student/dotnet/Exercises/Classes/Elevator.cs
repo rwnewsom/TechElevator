d0a8ffa89084e29fe8143c6ee144b761bddc07f9
Rob Newsom <rwnewsom@gmail.com>
2021-05-21 14:03:47 -0400

    all encapsulation exercises complete

--- module-1/09_Classes_Encapsulation/exercise-student/dotnet/Exercises/Classes/Elevator.cs
+++ module-1/09_Classes_Encapsulation/exercise-student/dotnet/Exercises/Classes/Elevator.cs
@@ -2,6 +2,53 @@
 {
     public class Elevator
     {
+        //PROPERTIES
+        public int CurrentLevel { get; private set; } = 1;
+
+        public int NumberOfLevels { get; private set; }
+
+        public bool DoorIsOpen { get; private set; }
+
+        //CONSTRUCTOR
+
+        public Elevator(int numberOfLevels)
+        {
+            this.NumberOfLevels = numberOfLevels;
+        }
+
+        //METHODS
+
+        public void OpenDoor()
+        {
+            this.DoorIsOpen = true;
+        }
+
+        public void CloseDoor()
+        {
+            this.DoorIsOpen = false;
+        }
+
+        public void GoUp(int desiredFloor)
+        {
+            if (!DoorIsOpen)
+            {
+                if ((desiredFloor <= this.NumberOfLevels) && (this.CurrentLevel<desiredFloor))
+                {
+                    this.CurrentLevel = desiredFloor;
+                }
+            }
+        }
+
+        public void GoDown(int desiredFloor)
+        {
+            if (!DoorIsOpen)
+            {
+                if ((desiredFloor <= this.NumberOfLevels) && (desiredFloor>0) && (this.CurrentLevel > desiredFloor))
+                {
+                    this.CurrentLevel = desiredFloor;
+                }
+            }
+        }
 
 
     }
--- module-1/09_Classes_Encapsulation/exercise-student/dotnet/Exercises/Classes/Television.cs
+++ module-1/09_Classes_Encapsulation/exercise-student/dotnet/Exercises/Classes/Television.cs
@@ -1,7 +1,100 @@
 ﻿namespace Exercises.Classes
 {
     public class Television
+
+
     {
+
+        //PROPERTIES
+
+        public bool IsOn { get; private set; } = false;
+
+        public int CurrentChannel { get; private set; } = 3;
+
+        public int CurrentVolume { get; private set; } = 2;
+
+
+        //METHODS
+
+        public void TurnOff()
+        {
+            this.IsOn = false;
+        }
+
+        public void TurnOn()
+        {
+            this.IsOn = true;
+            this.CurrentChannel = 3;
+            this.CurrentVolume = 2;
+        }
+
+        public void ChangeChannel(int newChannel)
+        {
+            if ((this.IsOn == true) && (newChannel >= 3) && (newChannel <= 18))
+            {
+                this.CurrentChannel = newChannel;
+            }
+        }
+
+        public void ChannelUp()
+        {
+            if (this.IsOn == true)
+            {
+                if ((this.CurrentChannel + 1) > 18)
+                {
+                    this.CurrentChannel = 3;
+                }
+                else
+                {
+                    this.CurrentChannel++;
+                }
+            }
+        }
+
+        public void ChannelDown()
+        {
+            if (this.IsOn == true)
+            {
+                if ((this.CurrentChannel - 1) < 3)
+                {
+                    this.CurrentChannel = 18;
+                }
+                else
+                {
+                    this.CurrentChannel--;
+                }
+            }
+        }
+
+        public void RaiseVolume()
+        {
+            if (this.IsOn == true)
+            {
+                if (this.CurrentVolume+1 > 10) 
+                {
+                    CurrentVolume = 10;
+                }
+                else
+                {
+                    CurrentVolume++;
+                }
+            }
+        }
+
+        public void LowerVolume()
+        {
+            if (this.IsOn == true)
+            {
+                if (this.CurrentVolume - 1 < 0)
+                {
+                    CurrentVolume = 0;
+                }
+                else
+                {
+                    CurrentVolume--;
+                }
+            }
+        }
 
 
     }
