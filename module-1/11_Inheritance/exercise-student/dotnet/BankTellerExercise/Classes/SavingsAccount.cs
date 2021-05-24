00b7690a02a338993f58957c556b7a2b48cccbe3
Rob Newsom <rwnewsom@gmail.com>
2021-05-24 16:14:35 -0400

    all tests now passing

--- module-1/11_Inheritance/exercise-student/dotnet/BankTellerExercise/Classes/SavingsAccount.cs
+++ module-1/11_Inheritance/exercise-student/dotnet/BankTellerExercise/Classes/SavingsAccount.cs
@@ -1,7 +1,46 @@
 ﻿namespace BankTellerExercise.Classes
 {
-    public class SavingsAccount
+    public class SavingsAccount : BankAccount
     {
 
+        //CONSTRUCTORS
+
+        public SavingsAccount(string accountHolderName, string accountNumber)
+        {
+            this.AccountHolderName = accountHolderName;
+            this.AccountNumber = accountNumber;
+            this.Balance = 0;
+        }
+
+        public SavingsAccount(string accountHolderName, string accountNumber, decimal balance)
+        {
+            this.AccountHolderName = accountHolderName;
+            this.AccountNumber = accountNumber;
+            this.Balance = balance;
+        }
+
+
+        //METHOD OVERRIDE
+
+        public override decimal Withdraw(decimal amountToWithdraw)
+        {
+            if ((this.Balance - amountToWithdraw) >= 150)
+            {
+                return base.Withdraw(amountToWithdraw);
+            } 
+            
+            else if ((this.Balance - amountToWithdraw) >= 2)
+            {
+                base.Withdraw(2);
+                return base.Withdraw(amountToWithdraw);
+            }
+
+            else
+            {
+                return base.Withdraw(0);
+            }
+            
+            //return base.Withdraw(amountToWithdraw);
+        }
     }
 }
