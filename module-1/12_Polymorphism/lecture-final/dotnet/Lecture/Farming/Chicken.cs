50494bd87cde97b1141f9bb8238ba64d4bbb1d4e
Matt Eland <matt.eland@gmail.com>
2021-05-25 12:11:06 -0400

    module-1/12_Polymorphism Lecture Final

---
+++ module-1/12_Polymorphism/lecture-final/dotnet/Lecture/Farming/Chicken.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lecture.Farming
+{
+    public class Chicken : FarmAnimal
+    {
+        public Chicken() : base("Chicken", "cluck")
+        {
+        }
+
+        public Chicken(string sound) : base("Chicken", sound)
+        {
+
+        }
+
+        public void LayEgg()
+        {
+            Console.WriteLine("Chicken laid an egg!");
+        }
+    }
+}
---
+++ module-1/12_Polymorphism/lecture-final/dotnet/Lecture/Farming/Tractor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lecture.Farming
+{
+    public class Tractor : ISingable
+    {
+        public string Name { get; } = "Tractor";
+        public string Sound { get; } = "*rumble / vroooom*";
+
+        public void PlowField()
+        {
+            Console.WriteLine("Farming away!");
+        }
+
+        public void BeginVerse()
+        {
+            Console.WriteLine("Behold the tale of the tractor!");
+        }
+    }
+}
