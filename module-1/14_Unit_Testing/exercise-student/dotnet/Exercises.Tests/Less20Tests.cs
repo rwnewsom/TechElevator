cda88ff40256331bfdd5671bbc670d6ff4b3ae66
Rob Newsom <rwnewsom@gmail.com>
2021-05-30 20:28:22 -0400

    half way done.

---
+++ module-1/14_Unit_Testing/exercise-student/dotnet/Exercises.Tests/Less20Tests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Exercises;
+
+namespace Exercises.Tests
+{
+    [TestClass]
+    public class Less20Tests
+    {
+        [TestMethod]
+        [DataRow(18,true)]
+        [DataRow(19, true)]
+        [DataRow(20, false)]
+        [DataRow(0, false)]
+        public void OnlyTrueIfOneOrTwoLessThanMultipleOfTwenty(int number, bool expected)
+        {
+            //Arrange
+            Less20 tryThree = new Less20();
+
+            //Act
+
+            bool result = tryThree.IsLessThanMultipleOf20(number);
+
+            //Assert
+
+            Assert.AreEqual(expected, result);
+
+        }
+
+    }
+}
---
+++ module-1/14_Unit_Testing/exercise-student/dotnet/Exercises.Tests/Lucky13Tests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Exercises;
+
+namespace Exercises.Tests
+{
+    [TestClass]
+    public class Lucky13Tests
+    {
+        [TestMethod]
+        [DataRow(new int[] { 1, 2, 3 }, false)]
+        [DataRow(new int[] { 0, 2, 4 }, true)]
+        [DataRow(new int[] { 1, 2, 4 }, false)]
+        public void ArrayWithOnesOrThreesShouldReturnFalse(int[] nums, bool expected)
+        {
+            //Arrange
+            Lucky13 unLucky = new Lucky13();
+
+            //Act
+
+            bool result = unLucky.GetLucky(nums);
+
+            //Assert
+            Assert.AreEqual(expected, result);
+
+
+
+        }
+    }
+}
