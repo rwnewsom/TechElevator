01c81cc38e4f2c7002b5a09034748ed1dad2df3c
Rob Newsom <rwnewsom@gmail.com>
2021-05-30 20:55:39 -0400

    three more to go

---
+++ module-1/14_Unit_Testing/exercise-student/dotnet/Exercises.Tests/MaxEnd3Tests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Exercises;
+
+namespace Exercises.Tests
+{
+    [TestClass]
+    public class MaxEnd3Tests
+    {
+        [TestMethod]
+        [DataRow(new int[] { 1, 2, 3 }, new int[] { 3, 3, 3 })]
+        [DataRow(new int[] { 0, 2, 4 }, new int[] { 4, 4, 4 })]
+        [DataRow(new int[] { 8, 2, 7 }, new int[] { 8, 8, 8 })]
+        [DataRow(new int[] { 2, 28, 9 }, new int[] { 9, 9, 9 })]
+        [DataRow(new int[] { -8, 2, -7 }, new int[] { -7, -7, -7 })]
+        public void ArrayWithOnesOrThreesShouldReturnFalse(int[] nums, int[] expected)
+        {
+            //Arrange
+            MaxEnd3 samesies = new MaxEnd3();
+
+            //Act
+
+            int[] result = samesies.MakeArray(nums);
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(expected.Length, result.Length);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], result[i]);
+            }
+            
+
+
+
+        }
+    }
+}
---
+++ module-1/14_Unit_Testing/exercise-student/dotnet/Exercises.Tests/NonStartTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Exercises;
+
+namespace Exercises.Tests
+{
+    [TestClass]
+    public class NonStartTests
+    {
+        [TestMethod]
+        [DataRow("Hello","There","ellohere")]
+        [DataRow("java", "code", "avaode")]
+        [DataRow("shotl", "lava", "hotlava")]
+        //[DataRow("shotl", "lava", "notlava")] *verifying that all DataRows were testing
+        public void TwoStringsShouldConcatenateWithoutFirstChars(string wordsOne, string wordsTwo, string expected)
+        {
+            
+            //Arrange
+            NonStart pasting = new NonStart();
+
+            //Act
+            string result = pasting.GetPartialString(wordsOne, wordsTwo);
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+
+
+    }
+}
