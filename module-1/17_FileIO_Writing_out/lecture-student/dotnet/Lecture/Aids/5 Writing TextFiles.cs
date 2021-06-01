c79bc20b8f3cb96c2eab9defdf05be940e1cc475
Matt Eland <matt.eland@gmail.com>
2021-06-01 15:32:08 -0400

    module-1/17_FileIO_Writing_out early lecture and exercise

---
+++ module-1/17_FileIO_Writing_out/lecture-student/dotnet/Lecture/Aids/5 Writing TextFiles.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace Lecture.Aids
+{
+    public static class WritingTextFiles
+    {
+        /*
+        * This method below provides sample code for printing out a message to a text file.
+        */
+        public static void WritingAFile()
+        {
+
+
+            // After the using statement ends, file has now been written
+            // and closed for further writing
+        }
+    }
+}
