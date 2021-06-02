cdfe069cb2d9ecf7ca43fde541d6b19d0a27e645
Rob Newsom <rwnewsom@gmail.com>
2021-06-02 07:55:37 -0400

    comments set for quiz game challenge

--- module-1/16_FileIO_Reading_in/exercise-student/dotnet/QuizMaker/Program.cs
+++ module-1/16_FileIO_Reading_in/exercise-student/dotnet/QuizMaker/Program.cs
@@ -6,7 +6,33 @@ namespace QuizMaker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Quiz Time!");
+
+            //access the quiz file, need user input prompt or hard code 
+            Console.WriteLine("Enter a complete file path to the quiz:");
+
+            //initialize variables for totalQuestions and totalScore
+            int totalQuestions = 0;
+            int totalScore = 0;
+
+            //catch bad file path or file
+            //open the file and parse the lines
+
+            //split each line into a string array
+
+            //loop through array to identify answer, assigning to an integer variable 
+
+            //Display question and each answer, prefacing answers with index number, stripping asterisks
+            
+
+            //catch non int entry or entry out of range (1-4)
+            //Prompt user to enter answer and compare with correct answer
+            //increment totalQuestions
+            //if correct answer, increment totalScore
+
+            //continue loop until end of file
+
+            //display final score by comparing total questions vs total correct answers
         }
     }
 }
