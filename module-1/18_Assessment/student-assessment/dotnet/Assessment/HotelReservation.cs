438776261c446a61dc3bb4a3d6428c674b88d1c7
Rob Newsom <rwnewsom@gmail.com>
2021-06-02 09:41:38 -0400

    Functional program, still need unit tests

---
+++ module-1/18_Assessment/student-assessment/dotnet/Assessment/HotelReservation.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assessment
+{
+    class HotelReservation
+    {
+        //PROPERTIES
+        public string Name { get; private set; }
+
+        public int NumberOfNights { get; private set; }
+
+        public double EstimatedTotal
+        {
+            get
+            {
+                return NumberOfNights * 59.99;
+            }
+        }
+
+        //CONSTRUCTORS
+
+        public HotelReservation(string name, int numberOfNights)
+        {
+            this.Name = name;
+            this.NumberOfNights = numberOfNights;
+        }
+
+        
+
+        //METHODS
+        /// <summary>
+        /// Calculate actual hotel bill.
+        /// </summary>
+        /// <param name="requiresCleaning">True if guest trashed the room.</param>
+        /// <param name="usedMinibar">True if guest touched the minibar, even if no PBR was removed.</param>
+        /// <returns>An exhorbinant bill.</returns>
+        public double CalculateActualTotal(bool requiresCleaning, bool usedMinibar)
+        {
+            double minibarFee = 12.99;
+            double cleaningFee = 34.99;
+            if (usedMinibar && requiresCleaning)
+            {
+                return this.EstimatedTotal + minibarFee + 2 * cleaningFee;
+            }
+
+            else if (usedMinibar)
+            {
+                return this.EstimatedTotal + minibarFee;
+            }
+
+            else if (requiresCleaning)
+            {
+                return this.EstimatedTotal + cleaningFee;
+            }
+            return EstimatedTotal;
+        }
+
+        public override string ToString()
+        {
+            return $"RESERVATION - {this.Name} - {this.EstimatedTotal}";
+        }
+
+        //public override string ToString()
+
+        //return;
+
+    }
+}
--- module-1/18_Assessment/student-assessment/dotnet/Assessment/Program.cs
+++ module-1/18_Assessment/student-assessment/dotnet/Assessment/Program.cs
@@ -8,8 +8,16 @@ namespace Assessment
         {
             // create an object and call methods on it (manual testing) from this class.
 
+            HotelReservation RobsRes = new HotelReservation("Rob", 4);
+            //Console.WriteLine(RobsRes.EstimatedTotal);
+            //Console.WriteLine(RobsRes.CalculateActualTotal(true,true));
+
+            Console.WriteLine(RobsRes.ToString()); 
+
             Console.WriteLine("Press any key to continue");
-            Console.ReadLine();
+            Console.ReadLine();  
         }
+
+        
     }
 }
