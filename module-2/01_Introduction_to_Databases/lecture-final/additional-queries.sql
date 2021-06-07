0d8b9df4ca85b2ac3b49633e5bbf4e7e97874acc
Matt Eland <matt.eland@gmail.com>
2021-06-07 13:41:26 -0400

    module-2/01 SQL - Lecture final

---
+++ module-2/01_Introduction_to_Databases/lecture-final/additional-queries.sql
@@ -0,0 +1,116 @@
+-- The name and population of all cities in the USA with a population of greater than 1 million people (9 rows)
+SELECT
+	name,
+	population
+FROM
+	city
+WHERE
+	population > 1000000
+	AND countrycode = 'USA'
+
+-- The name and population of all cities in China with a population of greater than 1 million people
+SELECT
+	name,
+	population
+FROM
+	city
+WHERE
+	population > 1000000
+	-- AND countrycode = 'CHN'
+	AND countrycode IN (SELECT code FROM country WHERE name = 'China')
+
+-- The name and region of all countries in North or South America
+SELECT
+	name,
+	region
+FROM
+	country
+WHERE
+	continent IN ('North America', 'South America')
+  
+-- The name, continent, and head of state of all countries whose form of government is a monarchy
+SELECT
+	name,
+	continent,
+	headofstate
+	--governmentform
+FROM
+	country
+WHERE
+	governmentform LIKE '%Monarchy%'
+
+-- The name, country code, and population of all cities with a population less than one thousand people
+SELECT
+	name,
+	countrycode,
+	population
+FROM
+	city
+WHERE
+	population < 1000
+
+-- The name and region of all countries in North or South America except for countries in the Caribbean
+SELECT
+	name,
+	region
+FROM
+	country
+WHERE
+	continent LIKE '%America%'
+	AND region <> 'Caribbean'
+
+-- The name, population, and GNP of all countries with a GNP greater than $1 trillion dollars and a population of less than 1 billion people
+SELECT
+	name,
+	population,
+	gnp
+FROM
+	country
+WHERE
+	gnp > 1000000 -- 1 gnp is $1 million
+	AND population < 1000000000
+
+-- The name and population of all cities in Texas that have a population of greater than 1 million people
+SELECT
+	name,
+	population
+FROM
+	city
+WHERE
+	district = 'Texas'
+	AND population > 1000000
+
+-- The name and average life expectancy of all countries in southern regions
+SELECT
+	name,
+	lifeexpectancy,
+	region
+FROM
+	country
+WHERE
+	region LIKE '%South%'
+
+-- The name and average life expectancy of all countries in southern regions for which an average life expectancy has been provided (i.e. not equal to null)
+SELECT
+	name,
+	lifeexpectancy
+FROM
+	country
+WHERE
+	region LIKE '%South%'
+	AND lifeexpectancy IS NOT NULL
+
+-- The name, continent, GNP, and average life expectancy of all countries in Africa or Asia that have an average life expectancy of at least 70 years and a GNP between $1 million and $100 million dollars
+SELECT
+	name,
+	continent,
+	gnp,
+	lifeexpectancy
+FROM
+	country
+WHERE
+	continent IN ('Africa', 'Asia')
+	AND lifeexpectancy >= 70
+	AND gnp BETWEEN 1000 AND 100000
+	--AND gnp >= 1000 -- gnp is in $1,000 units
+	--AND gnp <= 100000
