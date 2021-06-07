8cad415a96336a78291aca913389cb6bf90496fb
Rob Newsom <rwnewsom@gmail.com>
2021-06-07 15:33:41 -0400

    first five complete for module 2 db intro

--- module-2/01_Introduction_to_Databases/exercise-student/mssql/intro-to-databases-exercises.sql
+++ module-2/01_Introduction_to_Databases/exercise-student/mssql/intro-to-databases-exercises.sql
@@ -5,14 +5,56 @@
 --   The value immediately after the problem statement is the expected number of rows that should be returned by the query.
 
 -- 1. The name and population of all cities in Ontario, Canada (27 rows)
+SELECT
+	name,
+	population
+FROM
+	city
+WHERE
+district = 'Ontario';
+	
 
 -- 2. The name and population of all cities in Montana (1 row)
+SELECT
+	name,
+	population
+FROM
+	city
+WHERE
+district = 'Montana';
 
 -- 3. The name, form of government, and head of state of all countries in Europe (46 rows)
+SELECT
+	name,
+	governmentform,
+	headofstate
+	--continent
+FROM
+	country
+WHERE
+	continent = 'Europe';
+	
 
 -- 4. The name, population, surface area, and average life expectancy of all countries in Asia (51 rows)
+SELECT
+	name,
+	population,
+	surfacearea,
+	lifeexpectancy
+FROM
+	country
+WHERE
+	continent = 'Asia';
 
 -- 5. The name, country code, and population of all cities with a population greater than 8 million people (10 rows)
+SELECT
+	name,
+	countrycode,
+	population
+FROM
+	city
+WHERE
+	population > 8000000;
 
 -- 6. The name, country code, and population of all cities with a population less than one thousand people (11 rows)
 
--- module-2/01_Introduction_to_Databases/lecture-student/lecture-queries.sql
+++ module-2/01_Introduction_to_Databases/lecture-student/lecture-queries.sql
@@ -1,42 +1,130 @@
 -- SELECT ... FROM
--- Selecting the names for all countries
+SELECT * FROM country;
 
+-- Selecting the names for all countries
+SELECT name FROM country;
 
 -- Selecting the name and population of all countries
-
+SELECT name, population FROM country;
 
 -- Selecting all columns from the city table
+
+SELECT * FROM city;
 
 
 -- SELECT ... FROM ... WHERE
 -- Selecting the cities in Ohio
+SELECT name, district, countrycode FROM city WHERE district = 'Ohio';
+
+SELECT
+	name,
+	district,
+	countrycode
+FROM
+	city
+WHERE
+	district = 'Ohio';
 
 
 -- Selecting countries that gained independence in the year 1776
+SELECT
+	name
+FROM
+	country
+WHERE
+	indepyear = '1776';
 
 
 -- Selecting countries not in Asia
+SELECT
+	name, continent
+FROM
+	country
+WHERE
+	continent<>'Asia'; -- <> is the same as != in C#, former is the preferred syntax
 
 
 -- Selecting countries that do not have an independence year
 
+SELECT 
+	name, indepyear
+FROM
+	country
+WHERE
+	indepyear IS NULL;
+
+
 -- Selecting countries that do have an independence year
+
+SELECT
+	name
+FROM
+	country
+WHERE
+	indepyear IS NOT NULL
 
 
 -- Selecting countries that have a population greater than 5 million
+SELECT
+	name
+FROM
+	country
+WHERE
+	population > 5000000;
 
 
 
 -- SELECT ... FROM ... WHERE ... AND/OR
 -- Selecting cities in Ohio and Population greater than 400,000
 
+SELECT
+	name,
+	district,
+	population
+FROM
+	city
+WHERE
+	district = 'Ohio'
+	AND population > 400000;
+
+	
+
 -- Selecting country names on the continent North America or South America
+SELECT
+	name,
+	continent
+FROM
+	country
+WHERE
+	continent = 'North America'
+	OR continent = 'South America';
+
+	--Alternate syntax
+
+SELECT
+	name,
+	continent
+FROM
+	country
+WHERE
+	continent IN ('North America', 'South America', 'Antarctica');
+	--continent NOT IN ('North America', 'South America', 'Antarctica');
+
 
 
 
 
 -- SELECTING DATA w/arithmetic
 -- Selecting the population, life expectancy, and population per area
+SELECT
+	name as 'Country Name',
+	population,
+	lifeexpectancy,
+	surfacearea,
+	(population / surfacearea) as 'Density'
+FROM
+	country;
+
 --	note the use of the 'as' keyword
 
 
