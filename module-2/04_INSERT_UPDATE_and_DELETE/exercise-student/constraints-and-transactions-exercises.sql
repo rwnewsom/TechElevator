90e6c27754797f7dccb9a32c19c13b34e78e5f28
Rob Newsom <rwnewsom@gmail.com>
2021-06-13 15:23:49 -0400

    module 4 exercises complete

--- module-2/04_INSERT_UPDATE_and_DELETE/exercise-student/constraints-and-transactions-exercises.sql
+++ module-2/04_INSERT_UPDATE_and_DELETE/exercise-student/constraints-and-transactions-exercises.sql
@@ -29,45 +29,104 @@ INSERT INTO film_actor (actor_id, film_id)
 VALUES (202, 1001)
 
 -- 4. Add Mathmagical to the category table.
---SELECT * FROM category --original 16 - new=17
+SELECT * FROM category --original 16 - new=17
 INSERT INTO category (name)
 VALUES ('Mathmagical')
 
 -- 5. Assign the Mathmagical category to the following films, "Euclidean PI",
 -- "EGG IGBY", "KARATE MOON", "RANDOM GO", and "YOUNG LANGUAGE"
 --KM is 494, original cat is horror
---SELECT * FROM film_category WHERE film_id = 494
+--SELECT * FROM film WHERE title IN ('Euclidean PI', 'EGG IGBY', 'KARATE MOON', 'RANDOM GO', 'YOUNG LANGUAGE')--film_category --WHERE film_id = 494
+-- WHERE film_id IN (274,494,714,996,1001,1002)
 --SELECT * FROM category
+BEGIN TRANSACTION
+
+UPDATE
+	film_category
+SET
+	category_id = 17
+WHERE film_id IN (274,494,714,996,1001)
+
+
+COMMIT TRANSACTION
+
+--SELECT 
+	--title, name 
+--FROM 
+	--film f
+	--INNER JOIN film_category fc ON f.film_id = fc.film_id
+	--INNER JOIN category c ON fc.category_id = c.category_id 
+SELECT * FROM film
+
+--BEGIN TRANSACTION
+--DELETE FROM film WHERE film_id IN (1002,1003,1004)
+--COMMIT TRANSACTION
 
 -- 6. Mathmagical films always have a "G" rating, adjust all Mathmagical films
 -- accordingly.
 -- (5 rows affected)
+BEGIN TRANSACTION
+UPDATE
+	film
+SET
+	rating = 'G'
+WHERE film_id IN (274,494,714,996,1001)
+COMMIT TRANSACTION
+
 
 -- 7. Add a copy of "Euclidean PI" to all the stores.
+BEGIN TRANSACTION
 --SELECT * FROM film --filmid 1001
 --SELECT * FROM store --1 & 2
---INSERT INTO inventory (store_id, film_id) VALUES(1,1001)
---INSERT INTO inventory (store_id, film_id) VALUES(2,1001)
+INSERT INTO inventory (store_id, film_id) VALUES(1,1001)
+INSERT INTO inventory (store_id, film_id) VALUES(2,1001)
+COMMIT TRANSACTION
 
 -- 8. The Feds have stepped in and have impounded all copies of the pirated film,
 -- "Euclidean PI". The film has been seized from all stores, and needs to be
 -- deleted from the film table. Delete "Euclidean PI" from the film table.
 -- (Did it succeed? Why?)
--- <YOUR ANSWER HERE>
+BEGIN TRANSACTION
+DELETE FROM film WHERE title = 'Euclidean PI'
+ROLLBACK TRANSACTION
+-- <YOUR ANSWER HERE No due to database conflicts - some actors reference the film, in addition to store inventory>
+
 
 -- 9. Delete Mathmagical from the category table.
+--SELECT * FROM category
+BEGIN TRANSACTION
+DELETE FROM category WHERE category_id =17
+ROLLBACK TRANSACTION
 -- (Did it succeed? Why?)
--- <YOUR ANSWER HERE>
+-- <YOUR ANSWER HERE -No due to referential integrity - film category points to it>
 
 -- 10. Delete all links to Mathmagical in the film_category tale.
+--SELECT * FROM film_category WHERE category_id = 17
+BEGIN TRANSACTION
+DELETE FROM film_category WHERE category_id = 17
+COMMIT TRANSACTION
+
 -- (Did it succeed? Why?)
--- <YOUR ANSWER HERE>
+-- <YOUR ANSWER HERE YES as it involved a JOIN table.  FOr instance, Euclidean PI wasn't affected as it had no entry there.>
 
 -- 11. Retry deleting Mathmagical from the category table, followed by retrying
 -- to delete "Euclidean PI".
+BEGIN TRANSACTION
+DELETE FROM	category WHERE name = 'Mathmagical'
+COMMIT TRANSACTION
+--SELECT * FROM category
+
+BEGIN TRANSACTION
+DELETE FROM
+	film
+WHERE title = 'Euclidean PI'
+ROLLBACK TRANSACTION
 -- (Did either deletes succeed? Why?)
--- <YOUR ANSWER HERE>
+-- <YOUR ANSWER HERE I successfully deleted category as the action violated no constraints.  However deleting the film failed as two actors reference it.>
 
 -- 12. Check database metadata to determine all constraints of the film id, and
 -- describe any remaining adjustments needed before the film "Euclidean PI" can
 -- be removed from the film table.
+
+--film ID has 'Allow Nulls' set to FALSE - neccesary as it is a primary key.  In order to delete the film, you would need to essentially reverse all transactions.
+--remove it from the inventory of stores 1 & 2, and remove the actors who reference it. After that it could be deleted.
