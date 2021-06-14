6954b0bda956807823bf8c1b78aa0acd28dfe345
Matt Eland <matt.eland@gmail.com>
2021-06-14 12:23:13 -0400

    module-2/6 - DAO Lecture Final

---
+++ module-2/06_Database_Connectivity_DAO/lecture-final/dotnet/WorldGeography/DAL/CitySqlDAO.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using WorldGeography.Models;
+
+namespace WorldGeography.DAL
+{
+    public class CitySqlDAO : ICityDAO
+    {
+        private readonly string connectionString;
+
+        private const string SqlInsert = "INSERT INTO city (name, countrycode, district, population) VALUES (@name, @countrycode, @district, @population);";
+        private const string SqlSelect = "SELECT id, name, countrycode, district, population FROM city WHERE countryCode = @countryCode; SELECT @@IDENTITY;";
+
+        /// <summary>
+        /// Creates a new sql-based city dao.
+        /// </summary>
+        /// <param name="databaseconnectionString"></param>
+        public CitySqlDAO(string databaseconnectionString)
+        {
+            connectionString = databaseconnectionString;
+        }
+
+        public void AddCity(City city)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand command = new SqlCommand(SqlInsert, conn);
+                    command.Parameters.AddWithValue("@name", city.Name);
+                    command.Parameters.AddWithValue("@countrycode", city.CountryCode);
+                    command.Parameters.AddWithValue("@district", city.District);
+                    command.Parameters.AddWithValue("@population", city.Population);
+
+                    // Execute the query and get a single result. This will be the result of our "Select @@Identity" statement.
+                    // @@Identity refers to the ID of the last row that we created with our query, so it will be our city's primary key.
+                    int id = Convert.ToInt32(command.ExecuteScalar());
+
+                    Console.WriteLine("Just created city " + id);
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Could not add city: " + ex.Message);
+            }
+        }
+
+        public IEnumerable<City> GetCitiesByCountryCode(string countryCode)
+        {
+            List<City> cities = new List<City>();
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand command = new SqlCommand(SqlSelect, conn);
+                    command.Parameters.AddWithValue("@countryCode", countryCode);
+
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        City city = new City();
+
+                        city.CityId = Convert.ToInt32(reader["id"]);
+                        city.Name = Convert.ToString(reader["name"]);
+                        city.CountryCode = Convert.ToString(reader["countrycode"]);
+                        city.District = Convert.ToString(reader["district"]);
+                        city.Population = Convert.ToInt32(reader["population"]);
+
+                        cities.Add(city);
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Problem getting cities: " + ex.Message);
+            }
+
+            return cities;
+        }
+    }
+}
---
+++ module-2/06_Database_Connectivity_DAO/lecture-final/dotnet/WorldGeography/DAL/ICountryDAO.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using WorldGeography.Models;
+
+namespace WorldGeography.DAL
+{
+    public interface ICountryDAO
+    {
+        /// <summary>
+        /// Gets all countries.
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<Country> GetCountries();
+
+        /// <summary>
+        /// Gets all countries for a continent.
+        /// </summary>
+        /// <param name="continent">The continent to filter by.</param>
+        /// <returns></returns>
+        IEnumerable<Country> GetCountries(string continent);
+    }
+}
