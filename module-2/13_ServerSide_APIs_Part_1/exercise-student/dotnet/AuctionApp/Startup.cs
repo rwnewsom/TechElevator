2705cca9ab0381d984737e252ac7520d09f08423
Matt Eland <matt.eland@gmail.com>
2021-06-22 15:08:56 -0400

    module-2/13 - ASP .NET Swagger

--- module-2/13_ServerSide_APIs_Part_1/exercise-student/dotnet/AuctionApp/Startup.cs
+++ module-2/13_ServerSide_APIs_Part_1/exercise-student/dotnet/AuctionApp/Startup.cs
@@ -3,6 +3,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
+using System.Reflection;
 
 namespace AuctionApp
 {
@@ -19,6 +22,21 @@ namespace AuctionApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            // Adds swagger documentation file support. See https://docs.microsoft.com/en-us/aspnet/core/tutorials/getting-started-with-swashbuckle?view=aspnetcore-3.1&tabs=visual-studio for more details
+            services.AddSwaggerGen(s =>
+            {
+                s.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo()
+                {
+                    Version = "v1",
+                    Title = "ASP .NET Exercises API",
+                    Description = "Because it's Magic! (TM)"
+                });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                s.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,6 +46,13 @@ namespace AuctionApp
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Adds swagger documentation file. See https://docs.microsoft.com/en-us/aspnet/core/tutorials/getting-started-with-swashbuckle?view=aspnetcore-3.1&tabs=visual-studio for more details
+            app.UseSwagger();
+            app.UseSwaggerUI(s => {
+                s.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                s.RoutePrefix = string.Empty;
+            });
 
             app.UseHttpsRedirection();
 
