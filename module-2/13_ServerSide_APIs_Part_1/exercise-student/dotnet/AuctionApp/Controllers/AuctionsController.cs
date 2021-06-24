316ebe9edbe96e39c2c8359ef7a256699ac3388d
Rob Newsom <rwnewsom@gmail.com>
2021-06-23 21:49:05 -0400

    all auction controller tests passing

--- module-2/13_ServerSide_APIs_Part_1/exercise-student/dotnet/AuctionApp/Controllers/AuctionsController.cs
+++ module-2/13_ServerSide_APIs_Part_1/exercise-student/dotnet/AuctionApp/Controllers/AuctionsController.cs
@@ -23,21 +23,52 @@ namespace AuctionApp.Controllers
             }
         }
 
-        // return a list of all auctions
-
+        /// <summary>
+        /// Generates a list of auctions (search optional)
+        /// </summary>
+        /// <param name="title_like">optional title to search for</param>
+        /// <param name="currentBid_lte">optional value bid should not exceed</param>
+        /// <returns>A list of Auction objects</returns>
         [HttpGet("/auctions")]
-        public List<Auction> GetAllAuctions()
+        public List<Auction> GetAllAuctions(string title_like = "", double currentBid_lte = 0)
         {
-            return dao.List();
+            if (string.IsNullOrEmpty(title_like))
+            {
+                if (currentBid_lte == 0)
+                {
+                    return dao.List();
+                }
+                return dao.SearchByPrice(currentBid_lte);
+                
+            }
+            else
+            {
+                if(currentBid_lte == 0)
+                {
+                    return dao.SearchByTitle(title_like);
+                }
+                return dao.SearchByTitleAndPrice(title_like, currentBid_lte);
+            }
+
+
         }
 
-        //return a specific auction by id
+        /// <summary>
+        /// searches for a single auction by id
+        /// </summary>
+        /// <param name="id">the id to search for</param>
+        /// <returns>An Auction object</returns>
         [HttpGet("/auctions/{id}")]
         public Auction GetAuctionById(int id)
         {
             return dao.Get(id);
         }
 
+        /// <summary>
+        /// Creates a new auction
+        /// </summary>
+        /// <param name="auction">An auction object</param>
+        /// <returns>An auction object</returns>
         [HttpPost("/auctions")]
         public Auction CreateAuction(Auction auction)
         {
