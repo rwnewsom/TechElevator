0169357d4dc5db7a4e559558886ebf0add1c5218
Matt Eland <matt.eland@gmail.com>
2021-06-24 13:30:41 -0400

    module-2/14 - Lecture Final

---
+++ module-2/14_ServerSide_APIs_Part_2/lecture-final/server/dotnet/HotelReservations/Controllers/HotelsController.cs
@@ -0,0 +1,94 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using HotelReservations.Models;
+using HotelReservations.Dao;
+
+namespace HotelReservations.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class HotelsController : ControllerBase
+    {
+        private static IHotelDao hotelDao;
+        private static IReservationDao reservationDao;
+
+        public HotelsController(IHotelDao _hotelDao, IReservationDao _reservationDao)
+        {
+            hotelDao = _hotelDao;
+            reservationDao = _reservationDao;
+        }
+
+        [HttpGet("hotels")]
+        public ActionResult<List<Hotel>> ListHotels()
+        {
+            return hotelDao.List();
+        }
+
+        [HttpGet("hotels/{id}")]
+        public ActionResult<Hotel> GetHotel(int id)
+        {
+            Hotel hotel = hotelDao.Get(id);
+
+            // TODO: What if hotel is null? How do we return a 404?
+            if (hotel == null)
+            {
+                return NotFound("Could not find hotel with id " + id);
+            }
+
+            // TODO: What happens if we get an exception here?
+            /*
+            string notification = null;
+            string anotherString = notification.ToUpper();
+            */
+
+            return Ok(hotel); // Ok here is not needed. Returning hotel on its own also gives a 200 status code
+        }
+
+        [HttpGet("hotels/filter")]
+        public ActionResult<List<Hotel>> FilterByStateAndCity(string state, string city)
+        {
+            List<Hotel> filteredHotels = new List<Hotel>();
+
+            List<Hotel> hotels = hotelDao.List();
+            // return hotels that match state
+            foreach (Hotel hotel in hotels)
+            {
+                if (city != null)
+                {
+                    // if city was passed we don't care about the state filter
+                    if (hotel.Address.City.ToLower().Equals(city.ToLower()))
+                    {
+                        filteredHotels.Add(hotel);
+                    }
+                }
+                else
+                {
+                    if (hotel.Address.State.ToLower().Equals(state.ToLower()))
+                    {
+                        filteredHotels.Add(hotel);
+                    }
+                }
+            }
+            return filteredHotels;
+        }
+
+        [HttpGet("hotels/{hotelId}/reservations")]
+        public ActionResult<List<Reservation>> ListReservationsByHotel(int hotelId)
+        {
+            // TODO: What if the ID is negative?
+            if (hotelId <= 0)
+            {
+                return BadRequest("Enter a valid hotel id. Ids must be greater than 0."); // Status code 400
+            }
+
+            // TODO: What if they give us a hotel that doesn't exist?
+            Hotel hotel = hotelDao.Get(hotelId);
+            if (hotel == null)
+            {
+                return NotFound("There is no hotel with id " + hotelId);
+            }
+
+            return reservationDao.FindByHotel(hotelId);
+        }
+    }
+}
---
+++ module-2/14_ServerSide_APIs_Part_2/lecture-final/server/dotnet/HotelReservations/Models/Reservation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelReservations.Models
+{
+    public class Reservation
+    {
+        public int? Id { get; set; }
+
+        // TODO: Mark some of these fields as required
+
+        // TODO: What happens if the set is private?
+
+        public int HotelId { get; set; }
+
+        // TODO: Use a string length validation on FullName
+        [Required(ErrorMessage = "Who is this reservation for?", AllowEmptyStrings = false)]
+        [StringLength(255, MinimumLength = 2)]
+        public string FullName { get; set; }
+
+        [Required]
+        public string CheckinDate { get; set; }
+
+        [Required]
+        public string CheckoutDate { get; set; }
+
+        // TODO: Add range validation to this to restrict the guest count to between 1 and 5 guests
+        [Range(1, 8)]
+        public int Guests { get; set; }
+
+        public Reservation() // TODO: What happens if we don't have a parameterless reservation constructor?
+        {
+        }
+
+        public Reservation(int? id, int hotelId, string fullName, string checkinDate, string checkoutDate, int guests)
+        {
+            Id = id ?? new Random().Next(100, int.MaxValue);
+            HotelId = hotelId;
+            FullName = fullName;
+            CheckinDate = checkinDate;
+            CheckoutDate = checkoutDate;
+            Guests = guests;
+        }
+    }
+}
