fa4a4d20c69e2c1917fb405dd4a2f50fe9dfa302
Matt Eland <matt.eland@gmail.com>
2021-06-25 11:26:35 -0400

    module-2/15 - Matt's Progress 2

--- module-2/15_API_Review/FactsApp-Lecture-Final/Facts-Server/Facts-Server/Controllers/FactsController.cs
+++ module-2/15_API_Review/FactsApp-Lecture-Final/Facts-Server/Facts-Server/Controllers/FactsController.cs
@@ -18,6 +18,24 @@ namespace Facts_Server.Controllers
             facts = new FactsDao();
         }
 
-        
+        // 1. Add a new GET method to handle gets to /facts
+        // This should return the results of the DAO's GetAllFacts method
+        [HttpGet()]
+        public ActionResult<List<ChuckNorrisFact>> GetFactsList()
+        {
+            return facts.GetAllFacts();
+        }
+
+
+        // 2. Add a method to allow folks to POST a new fact.
+        // This should call the DAO's AddFact method with the new fact
+        [HttpPost()]
+        public ActionResult<ChuckNorrisFact> AddNewFact(ChuckNorrisFact newFact)
+        {
+            ChuckNorrisFact createdFact = facts.AddFact(newFact);
+
+            return Created($"/facts/{createdFact.Id}", newFact);
+        }
+
     }
 }
--- module-2/15_API_Review/FactsApp-Lecture-Final/Facts-Server/Facts-Server/FactsDao.cs
+++ module-2/15_API_Review/FactsApp-Lecture-Final/Facts-Server/Facts-Server/FactsDao.cs
@@ -14,10 +14,29 @@ namespace Facts_Server
         {
             facts = new List<ChuckNorrisFact>();
 
-            facts.Add(new ChuckNorrisFact(1, "Chuck's legs are named law and order"));
-            facts.Add(new ChuckNorrisFact(2, "Chuck's keyboard has 3 keys: 0, 1, and Deploy to Production"));
-
-            nextId = facts.Count + 1;
+            facts.Add(new ChuckNorrisFact(nextId++, "Chuck's legs are named law and order"));
+            facts.Add(new ChuckNorrisFact(nextId++, "Chuck's keyboard has 3 keys: 0, 1, and Deploy to Production"));
+            facts.Add(new ChuckNorrisFact(nextId++, "Chuck Norris’s keyboard doesn’t have a Ctrl key because nothing controls Chuck Norris."));
+            facts.Add(new ChuckNorrisFact(nextId++, "COVID-19 is in quarantine after exposure to Chuck Norris"));
+            facts.Add(new ChuckNorrisFact(nextId++, "Chuck Norris does not use a debugger since his code is perfect. However, if he ever made a mistake (he won't), he'd simply stare the bugs into submission."));
+            facts.Add(new ChuckNorrisFact(nextId++, "Chuck Norris can read regular expressions"));
+            facts.Add(new ChuckNorrisFact(nextId++, "Microsoft DID ask Chuck Norris"));
+            facts.Add(new ChuckNorrisFact(nextId++, "Chuck Norris is what makes Polymorphism possible"));
+            facts.Add(new ChuckNorrisFact(nextId++, "When Chuck Norris throws exceptions, it’s across the room."));
+            facts.Add(new ChuckNorrisFact(nextId++, "All arrays Chuck Norris declares are of infinite size, because Chuck Norris knows no bounds."));
+            facts.Add(new ChuckNorrisFact(nextId++, "Chuck Norris writes code that optimizes itself."));
+            facts.Add(new ChuckNorrisFact(nextId++, "Chuck Norris doesn’t need garbage collection because he doesn’t call .Dispose(), he calls .DropKick()."));
+            facts.Add(new ChuckNorrisFact(nextId++, "The only pattern Chuck Norris knows is God Object."));
+            facts.Add(new ChuckNorrisFact(nextId++, "Project managers never ask Chuck Norris for estimation... ever."));
+            facts.Add(new ChuckNorrisFact(nextId++, "Chuck Norris doesn’t use web standards as the web will conform to him."));
+            facts.Add(new ChuckNorrisFact(nextId++, "Chuck Norris can unit test entire applications with a single assert."));
+            facts.Add(new ChuckNorrisFact(nextId++, "Chuck Norris CAN divide by 0."));
+            facts.Add(new ChuckNorrisFact(nextId++, "Chuck Norris can overflow your stack just by looking at it."));
+            facts.Add(new ChuckNorrisFact(nextId++, "To Chuck Norris, everything contains a vulnerability."));
+            facts.Add(new ChuckNorrisFact(nextId++, "Chuck Norris can access private methods."));
+            facts.Add(new ChuckNorrisFact(nextId++, "Chuck Norris can instantiate abstract classes and interfaces."));
+            facts.Add(new ChuckNorrisFact(nextId++, "Chuck Norris can inherit from sealed classes."));
+            facts.Add(new ChuckNorrisFact(nextId++, "All baristas know Java in fear that one day Chuck Norris walks in and demands it."));
         }
 
         public List<ChuckNorrisFact> GetAllFacts()
