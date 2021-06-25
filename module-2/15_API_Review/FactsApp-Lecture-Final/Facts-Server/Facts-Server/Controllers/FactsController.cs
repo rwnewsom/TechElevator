b28a6ba931d9d63f2bbc976bcfb8beaeda89d19e
Matt Eland <matt.eland@gmail.com>
2021-06-25 11:01:02 -0400

    module-2/14 - Matt's Progress 1

---
+++ module-2/15_API_Review/FactsApp-Lecture-Final/Facts-Server/Facts-Server/Controllers/FactsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Facts_Server.Controllers
+{
+    [Route("facts")]
+    [ApiController]
+    public class FactsController : ControllerBase
+    {
+        private FactsDao facts;
+
+        public FactsController()
+        {
+            facts = new FactsDao();
+        }
+
+        
+    }
+}
---
+++ module-2/15_API_Review/FactsApp-Lecture-Final/Facts-Server/Facts-Server/FactsDao.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Facts_Server
+{
+    public class FactsDao
+    {
+        private List<ChuckNorrisFact> facts;
+        private int nextId;
+
+        public FactsDao()
+        {
+            facts = new List<ChuckNorrisFact>();
+
+            facts.Add(new ChuckNorrisFact(1, "Chuck's legs are named law and order"));
+            facts.Add(new ChuckNorrisFact(2, "Chuck's keyboard has 3 keys: 0, 1, and Deploy to Production"));
+
+            nextId = facts.Count + 1;
+        }
+
+        public List<ChuckNorrisFact> GetAllFacts()
+        {
+            return facts;
+        }
+
+        public ChuckNorrisFact AddFact(ChuckNorrisFact fact)
+        {
+            fact.Id = nextId;
+            nextId++;
+
+            facts.Add(fact);
+
+            return fact;
+        }
+
+    }
+}
