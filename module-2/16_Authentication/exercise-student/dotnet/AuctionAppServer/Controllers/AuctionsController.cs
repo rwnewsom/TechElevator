51501dfb550a275ebe45b638b384e5ad5e0abfd2
Rob Newsom <rwnewsom@gmail.com>
2021-06-29 20:08:33 -0400

    Who am I? My name Jean Valjean!

--- module-2/16_Authentication/exercise-student/dotnet/AuctionAppServer/Controllers/AuctionsController.cs
+++ module-2/16_Authentication/exercise-student/dotnet/AuctionAppServer/Controllers/AuctionsController.cs
@@ -97,7 +97,7 @@ namespace AuctionApp.Controllers
         [HttpGet("whoami")]
         public ActionResult WhoAmI()
         {
-            return Ok("");
+            return Ok(User.Identity.Name); //My Name is Jean 
         }
     }
 }
