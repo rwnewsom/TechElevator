dbb4ac26d4841210afe7fce5713abecdce8744aa
Matt Eland <matt.eland@gmail.com>
2021-07-12 17:11:46 -0400

    module-3/6 - Add LINQ examples

---
+++ module-3/06_JavaScript_Functions/lecture-student/linq-samples/MattEland.LinqSamples/Book.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace MattEland.LinqSamples
+{
+    public struct Book
+    {
+        public string Title { get; }
+        public string Author { get; }
+        [JsonConverter(typeof(StringEnumConverter))]
+        public Genre Genre { get; }
+        public decimal Price { get; }
+        public IEnumerable<string> Characters { get; }
+
+        public bool IsFiction => Genre != Genre.Technical;
+
+        public Book(string title, string author, Genre genre, decimal price, IEnumerable<string> characters)
+        {
+            Title = title;
+            Author = author;
+            Genre = genre;
+            Characters = characters;
+            Price = price;
+        }
+
+    }
+}
