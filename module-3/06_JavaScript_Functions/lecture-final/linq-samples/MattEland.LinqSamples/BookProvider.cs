6e51f2e8717dbea513fa47e55b0f19ba45b6499d
Matt Eland <matt.eland@gmail.com>
2021-07-13 14:31:52 -0400

    module-3/6 - Lecture Final

---
+++ module-3/06_JavaScript_Functions/lecture-final/linq-samples/MattEland.LinqSamples/BookProvider.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace MattEland.LinqSamples
+{
+    public class BookProvider
+    {
+        private Lazy<IEnumerable<Book>> _books = new Lazy<IEnumerable<Book>>(GetBooks());
+        public IEnumerable<Book> Books => _books.Value;
+
+        private static IEnumerable<Book> GetBooks([CallerMemberName] string caller = "")
+        {
+            var books = new List<Book>
+            {
+                new Book("Sphere", "Michael Crichton", Genre.ScienceFiction, 9.99M, new List<string>()
+                {
+                    "Harry",
+                    "Norman",
+                    "Beth",
+                    "Jerry"
+                }),
+                new Book("Jurassic Park", "Michael Crichton", Genre.ScienceFiction, 14.99M, new List<string>()
+                {
+                    "Malcolm",
+                    "Grant",
+                    "Satler",
+                    "Nedry",
+                    "Hammond",
+                    "Gennaro",
+                    "Tim",
+                    "Lex"
+                }),
+                //new Book("Timeline", "Michael Crichton", Genre.ScienceFiction),
+                //new Book("Terminal Man", "Michael Crichton", Genre.ScienceFiction),
+                //new Book("The Great Train Robbery", "Michael Crichton", Genre.Fiction),
+                //new Book("Windows Presentation Foundation Unleashed", "Adam Nathan", Genre.Technical),
+                new Book("Working Effectively with Legacy Code", "Michael Feathers",  Genre.Technical, 45.99M, new List<string>()),
+                new Book("Your Code as a Crime Scene", "Adam Tornhill", Genre.Technical, 35.99M, new List<string>()),
+                new Book("Software Design X-Rays", "Adam Tornhill", Genre.Technical, 32.99M, new List<string>())
+            };
+
+            return books;
+        }
+    }
+}
---
+++ module-3/06_JavaScript_Functions/lecture-final/linq-samples/MattEland.LinqSamples/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MattEland.LinqSamples
+{
+    class Program
+    {
+        static void Main()
+        {
+            BookProvider dataProvider = new BookProvider();
+
+            IEnumerable<Book> books = dataProvider.Books.OrderBy(book => book.Title);
+
+            // FirstOrDefault - finds the first book matching the query, or returns null
+            Book favoriteBook = books.FirstOrDefault(book => book.Title == "Sphere");
+
+            // First - finds the first book matching the query or throws an exception
+            Book dinoBook = books.First(book => book.Title == "Jurassic Park");
+
+            // Count - Get a number of things that match the query
+            int numBooks = books.Count(book => book.Genre == Genre.Technical);
+
+            // Filter - take many things and omit things that don't match the expression
+            books = books.Where(book => book.Genre == Genre.Technical);
+
+            // Min - Equivalent for reduce with find smallest logic
+            decimal cheapestPrice = books.Min(book => book.Price); 
+
+            // Max - Equivalent for reduce with find largest logic
+            decimal largestPrice = books.Max(book => book.Price);
+
+            Console.WriteLine("The books matching your search are:");
+
+            // Select - Equivalent of Map
+            foreach (string title in books.Select(book => book.Title))
+            {
+                Console.WriteLine(title);
+            }
+
+            Console.WriteLine($"There are {numBooks} book(s) with the cheapest price being {cheapestPrice:C} and the most expesive being {largestPrice:C}");
+        }
+    }
+}
