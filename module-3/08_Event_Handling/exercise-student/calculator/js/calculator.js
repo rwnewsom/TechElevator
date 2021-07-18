0421d9762102fa47e6f11abbf59fb936fccad84e
Rob Newsom <rwnewsom@gmail.com>
2021-07-18 18:52:35 -0400

    finally got numbers!

--- module-3/08_Event_Handling/exercise-student/calculator/js/calculator.js
+++ module-3/08_Event_Handling/exercise-student/calculator/js/calculator.js
@@ -72,14 +72,19 @@ function clear() {
 }
 
 // add event listener for when the DOM is loaded
-document.addEventListener('LOADED_EVENT_GOES_HERE', () => {
+document.addEventListener('DOMContentLoaded', () => {
 
   // set the variable called display equal to the display element
   // HINT: use its id #display to get a reference to it
+  display = document.getElementById('display');
 
   // get a reference to all of the numbers
   // loop over each of the numbers
   // add a click event listener to each number to call the function clickNumber
+  const numbers = document.querySelectorAll('button.number');
+  numbers.forEach((number) => {
+    number.addEventListener('click', event => clickNumber(event));    
+  });
 
   // get a reference to the decimal point button
   // add a click event listener to call the function clickNumber
