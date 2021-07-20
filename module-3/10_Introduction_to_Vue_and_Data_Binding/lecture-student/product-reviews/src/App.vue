af74d2af6f3fd528b049532f0035254e3e5a990d
Rob Newsom <rwnewsom@gmail.com>
2021-07-19 20:52:50 -0400

    lecture notes

---
+++ module-3/10_Introduction_to_Vue_and_Data_Binding/lecture-student/product-reviews/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <product-reviews />
+    <!-- <ProductReviews />  -->
+    <h1>Hi Vue.js</h1>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Hello .NET Purple"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import ProductReviews from './components/ProductReviews.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    ProductReviews
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1{
+  color: rgb(71,11,68);
+}
+</style>
