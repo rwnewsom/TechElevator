a7f20918565a0293c835366136b6635857ef95df
Matt Eland <matt.eland@gmail.com>
2021-07-22 18:41:59 -0400

    module-3/13 - Prep work for tomorrow

--- module-3/13_Vue_Routing/lecture-student/src/App.vue
+++ module-3/13_Vue_Routing/lecture-student/src/App.vue
@@ -1,9 +1,18 @@
 <template>
-  <div></div>
+  <div id="app">
+    <product-review></product-review>
+  </div>
 </template>
 
 <script>
-export default {};
+import ProductReview from "./components/ProductReview.vue";
+
+export default {
+  name: "app",
+  components: {
+    ProductReview
+  }
+};
 </script>
 
 <style>
@@ -15,5 +24,15 @@ export default {};
   color: #2c3e50;
   max-width: 800px;
   margin: 60px auto 0 auto;
+}
+th, td {
+  text-align: left;
+}
+td {
+  padding-right: 10px;
+  vertical-align: top;
+}
+tr:nth-child(even) {
+  background-color: rgb(238, 238, 238);
 }
 </style>
