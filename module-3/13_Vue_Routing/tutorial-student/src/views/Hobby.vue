9fb5a9226fc8246ce3e0ee020ffa731eb0639478
Rob Newsom <rwnewsom@gmail.com>
2021-07-24 12:40:32 -0400

    tutorial

---
+++ module-3/13_Vue_Routing/tutorial-student/src/views/Hobby.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <h1>My Current Hobbies</h1>
+      <p>I like to code, run, watch sports (Columbus Crew, Columbus Blue Jackets).</p>
+      <p>I also play a video game called Blood Bowl. </p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
